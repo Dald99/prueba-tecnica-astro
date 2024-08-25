54bc7be870f1c7694a6107b84d80ac30e21abfe8
Dald99 <dani_lara99@hotmail.com>
2024-08-24 18:40:06 -0700

    Action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dald99.github.io',
+    base: '/prueba-tecnica-astro/',
+})
